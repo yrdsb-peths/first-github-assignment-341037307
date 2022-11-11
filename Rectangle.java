b0e0b1800b63a4ace06860b1454e0703aadde799
341037307 <117941837+341037307@users.noreply.github.com>
2022-11-11 14:34:45 -0500

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle
+{
+  private int length;
+  private int width;
+  
+  public Rectangle(int Length, int Width)
+  {
+    length = Length;
+    width = Width;
+  }
+  public int GetLength()
+  {
+    return length;
+  }
+  public int GetWidth()
+  {
+    return width;
+  }
+  public toString()
+  {
+    return "Rectange length is: " +  length + " Rectangle width is: " + width;
+    
+  }
+}
